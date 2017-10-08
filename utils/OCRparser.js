41267dddc3be590c6e4f5af1d1662d64420a99c3
jrmesser <messer.jr@gmail.com>
2017-10-08 15:13:38 -0700

    updated OCRparser to deal with more unwanted characters, handle single digit item prices, and deal with empty results

--- utils/OCRparser.js
+++ utils/OCRparser.js
@@ -1,8 +1,8 @@
 module.exports = inputText => {
 
-    const regex = /\d+.?\d+$/; //regex matching decimal value at end of line
+    const regex = /\d+.?\d*$/; //regex matching decimal value at end of line
 
-    return inputText.replace(/[|]+/g,'').split('\n').map(line => {
+    return inputText.replace(/[|'"]+/g,'').split('\n').map(line => {
         line = line.trim(); //trim whitespace
         let index = line.search(regex); //returns index of beginning of match
         return [line.substring(0,index).trim(), line.substring(index).trim()]; //return array of two trimmed strings
