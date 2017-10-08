b3226527ebe0a91544cbce4305b178080c0a442e
jrmesser <messer.jr@gmail.com>
2017-10-08 16:09:23 -0700

    removed console.logs, added in some error checking

--- utils/splitItems.js
+++ utils/splitItems.js
@@ -7,15 +7,24 @@ module.exports = inputTransaction => {
         let runningTotals = {};// dict for storing running totals for person in party array
         inputTransaction.items.forEach(item => {//go through each item in items
             const itemShare = item.price / item.person.length;//calculate cost of the item for each person splitting it
-            console.log('itemShare', itemShare);
             item.person.forEach(person => {//go through the people splitting the item and update their totals
                 runningTotals[person] = runningTotals[person] ? runningTotals[person] += itemShare : runningTotals[person] = itemShare;//increment if person already owes something, otherwise initialize
             });
         });
-        console.log('runningTotals:', runningTotals);
         inputTransaction.party.forEach(person => {//loop through party and update totals with values from runningTotals
-            person.total = runningTotals[person.personId];
+            if (runningTotals[person.personId] === undefined) {
+                throw 'party not found';//make sure the person is in the runningTotals dict
+                return false;
+            }
+            else{ 
+                person.total = runningTotals[person.personId];
+                runningTotals[person.personId] = undefined; //clean up dict
+            }
         });
+        if (Object.keys(runningTotals) != []) {
+            throw 'items were split outside the party';
+            return false;
+        }
     }
     return inputTransaction;
 };
